003118e4028adf847b40a39c80de4a8705c9b1f4
PC6live <740687544@outlook.com>
2022-04-08 21:41:04 +0800

    chore: update

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,4 +11,5 @@
 #
 
 # Modify default IP
-#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+cd lede/package/lean && rm -rf luci-theme-argon && git clone https://github.com/jerrykuku/luci-theme-argon.git  
